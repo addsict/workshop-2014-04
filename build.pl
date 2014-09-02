80ea6a32d55b5e37e3a81d16fd088c10d13bf796
nqounet <mail@nqou.net>
2014-09-02 17:12:24 +0900

    README修正

--- build.pl
+++ build.pl
@@ -16,15 +16,16 @@ my @slides  = qw/
   regexp_and_replace.md
   regexp_and_meta.md
   regexp_and_options.md
+  qanda.md
   outro.md
   /;
 my $slide
-  = "<!-- このファイルは直接編集せずに src ディレクトリ内を編集し build.pl を実行してください。 -->\n";
+  = "<!-- このファイルは直接編集せずに src ディレクトリ内のファイルを編集し build.pl を実行してください。 -->\n";
 $slide .= join(
     "\n---\n",
     map {
         my $str = path("$src_dir/$_")->slurp_utf8;
-        $str =~ s/\n(##?)/\n___\n$1/g;
+        $str =~ s/\n(##? )/\n___\n$1/g;
         $str
     } @slides
 );
